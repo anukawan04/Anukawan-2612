f75e2d8331ed79a63b29b00f6abd056d5d168dfd
anukawan04 <anukawan2024@gmail>
2025-07-12 21:01:05 +0545

    cls

--- function.py
+++ function.py
@@ -1,5 +1,5 @@
 # funtion -block of code executed one at the time
-# def function_name():
+print("hello world")
 # codeee
 # def sum(a,b):  a= parameter
 #     # total = a +b  5= arugument
@@ -9,3 +9,11 @@
 
 
 # returnn - researchh
+
+# To get output from a function.
+
+# To use that output somewhere else in your program.
+
+# Functions without return will return None by default.
+
+# ###
--- loop.py
+++ loop.py
@@ -1,6 +1,8 @@
-fruits = ["mango, banana ,guava, litchi, grape, peach, kiwi, orange"]
+fruit = ["mango, banana ,guava, litchi, grape, peach, kiwi, orange"]
 for i in range(1, 9):
     print(i)
 
-for fruit in fruits:
-    print(fruit)
+for fruits in fruit:
+    print(fruits)
+
+    
