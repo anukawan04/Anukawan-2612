f52e29313ebfce9ea886bdf7046f903677db17c2
anukawan04 <anukawan2024@gmail>
2025-07-12 20:58:11 +0545

    cls

--- conditionals.py
+++ conditionals.py
@@ -25,3 +25,7 @@ else:
     print("you are not okay to drive") 
 
     # and and or
+
+# and Returns True if both statements are true	x < 5 and x < 10
+# or Returns True if one of the statements is true	x < 5 or x < 4
+# not Reverse the result, returns False if the result is true not (x < 5 and x < 10)
